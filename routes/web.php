b93c5d8434b3442919617ebf6caf621f5e1ee903
Ana Tara <ana.tara@outlook.com>
2025-03-29 22:53:19 -0400

    app supports images, DB updated with appropriate tables

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('English.index');
 });
-
+ 
 
 // English Routes
 Route::prefix('English')->name('English.')->group(function () {
